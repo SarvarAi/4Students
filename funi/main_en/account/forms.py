461b4ba51fb88d4602922fc56ac383fd36536b47
SarvarAi <sarvarislamov001@gmail.com>
2023-07-02 22:50:55 +0500

    basic commit

--- funi/main_en/account/forms.py
+++ funi/main_en/account/forms.py
@@ -60,3 +60,62 @@ class Signup3Form(forms.Form):
         'id': 'floatingPassword',
         'placeholder': 'Confirm your password'
     }))
+
+
+class AccountSettingsForm(forms.Form):
+    first_name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'type': 'text'
+    }))
+
+    last_name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'type': 'text'
+    }))
+
+    middle_name = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'type': 'text'
+    }))
+
+    email = forms.EmailField(max_length=255, widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+    }))
+
+    extra_email = forms.EmailField(max_length=255, required=False, widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+    }))
+
+    telephone_number = forms.CharField(max_length=255, widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+
+    extra_telephone_number = forms.CharField(max_length=255, required=False, widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+
+    bio = forms.CharField(max_length=255, widget=forms.Textarea(attrs={
+        'class': 'form-control',
+        'rows': '4'
+    }))
+
+
+class PasswordSettingsForm(forms.Form):
+    old_password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'type': 'password'
+    }))
+
+    new_password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'type': 'password'
+    }))
+
+    confirm_password = forms.CharField(max_length=255, widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'type': 'password'
+    }))
+
+
+class DetailedCheckForm(forms.Form):
+    ...
